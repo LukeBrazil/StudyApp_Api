5dcda2df59f0f07521d07481eb829d5940db0e69
Luke Brazil <Luke.brazil13@icloud.com>
2020-11-03 10:57:12 -0600

    added heroku

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const hostname = '127.0.0.1';
+const port = 5000;
+const server = http.createServer(app);
 
-var app = express();
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
