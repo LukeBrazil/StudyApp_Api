aebf9bea9fb6bbbb886c10ae0c72edbd2dfca930
Shaiah Wren <sde2111@columbia.edu>
2020-11-04 12:02:49 -0500

    users route added code

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const { route } = require('.');
 const router = express.Router();
 
 const UserModel = require('../models/usersModel');
 
-router.get('/all', async (req, res) => {
-  const allUsers = await UserModel.getAllUsers();
-  res.json(allUsers).status(200)
-})
+// Get All Users
+
+
+// Get All Users by ID
+
+router.get('/:id', async (req, res) => {
+  const user = await UserModel.findById(req.params.id);
+  res.json(user).status(200)
+});
+
+
+//Create User
+router.post('/', async (req, res))
+
+
+
+
+
+
+
+
 
 module.exports = router;
