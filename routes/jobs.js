80a8cdefafb1b998ba165f17b7af4f18bbe3fc63
Luke Brazil <Luke.brazil13@icloud.com>
2020-11-11 11:05:56 -0600

    testing axios get

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,9 +5,8 @@ router.get('/javascript', async (req, res) => {
    await axios.get('https://jobs.github.com/positions.json?description=javascript&location=us')
         .then(response => response.json())
         .then(data => {
-        const jobsData = data;
+        res.json(data)
         })
-        res.json(jobsData)
 
 })
 
